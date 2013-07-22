a824ac7a75cf2635f8c188b26377c4c59fbc6665
Mateus Maso <m.maso25@gmail.com>
2013-07-22 13:03:53 -0700

    Updated handlebars.nested.js

--- handlebars.nested.js
+++ handlebars.nested.js
@@ -1,30 +1,34 @@
-Handlebars.registerHelper = function(name, fn, inverse) {
-	var nestedFn = function() {
-		var _this = this;
-		var _arguments = [];
+(function(Handlebars) {
 
-		_.each(arguments, function(argument, i) {
-			if (_.isObject(argument) && argument.hash) {
-				_arguments[i] = _.clone(argument)
-				_.each(_arguments[i].hash, function(value, key) {
-					_arguments[i].hash[key] = Handlebars.resolveNested.apply(_this, [value])
-				});
-			} else {
-				_arguments[i] = Handlebars.resolveNested.apply(_this, [argument])
-			}
-		});
+	Handlebars.registerHelper = function(name, fn, inverse) {
+		var nestedFn = function() {
+			var _this = this;
+			var _arguments = [];
 
-		return fn.apply(this, _arguments);
-	}
+			_.each(arguments, function(argument, i) {
+				if (_.isObject(argument) && argument.hash) {
+					_arguments[i] = _.clone(argument)
+					_.each(_arguments[i].hash, function(value, key) {
+						_arguments[i].hash[key] = Handlebars.resolveNested.apply(_this, [value])
+					});
+				} else {
+					_arguments[i] = Handlebars.resolveNested.apply(_this, [argument])
+				}
+			});
 
-  if (inverse) nestedFn.not = inverse;
-  this.helpers[name] = nestedFn;
-};
+			return fn.apply(this, _arguments);
+		}
 
-Handlebars.resolveNested = function(value) {
-  if(_.isString(value) && value.indexOf('{{') >= 0) {
-    value = Handlebars.compile(value)(this);
-  }
-	
-  return value;
-};
+	  if (inverse) nestedFn.not = inverse;
+	  this.helpers[name] = nestedFn;
+	};
+
+	Handlebars.resolveNested = function(value) {
+	  if(_.isString(value) && value.indexOf('{{') >= 0) {
+	    value = Handlebars.compile(value)(this);
+	  }
+		
+	  return value;
+	};
+
+})(Handlebars);
